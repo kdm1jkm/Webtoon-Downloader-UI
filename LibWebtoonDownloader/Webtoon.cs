89536cab2a73ae279e39b219db9f340478f29726
kdm1jkm <kdm1jkmm@gmail.com>
2019-11-17 15:13:43 +0900

    초기 커밋.

---
+++ LibWebtoonDownloader/Webtoon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HtmlAgilityPack;
+
+namespace LibWebtoonDownloader
+{
+    class Webtoon
+    {
+        private int titleId;
+
+        private int startNo, endNo;
+
+        public Webtoon()
+        {
+            titleId = startNo = endNo = -1;
+        }
+
+        public Webtoon(int titleId)
+        {
+            this.titleId = titleId;
+            startNo = endNo = -1;
+        }
+
+        public Webtoon(int titleId, int no)
+        {
+            this.titleId = titleId;
+            startNo = endNo = no;
+        }
+
+        public Webtoon(int )
+    }
+}
