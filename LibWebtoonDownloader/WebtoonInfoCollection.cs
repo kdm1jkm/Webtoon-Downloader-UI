c5bf7885eadb6d002871a6329f814f084c8e87b7
kdm1jkm <kdm1jkmm@gmail.com>
2020-01-17 17:52:38 +0900

    WebtoonInfoCollection 완성

---
+++ LibWebtoonDownloader/WebtoonInfoCollection.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace LibWebtoonDownloader
+{
+    class WebtoonInfoCollection : List<WebtoonInfo>
+    {
+        public void Save(string fileName)
+        {
+            using(Stream ws = new FileStream(fileName, FileMode.Create))
+            {
+                BinaryFormatter serializer = new BinaryFormatter();
+                serializer.Serialize(ws, this);
+            }
+        }
+
+        public static WebtoonInfoCollection Load(string fileName)
+        {
+            if(File.Exists(fileName))
+            {
+                using(Stream rs = new FileStream(fileName, FileMode.Open))
+                {
+                    BinaryFormatter deserializer = new BinaryFormatter();
+                    return (WebtoonInfoCollection)deserializer.Deserialize(rs);
+                }
+            }
+            else
+            {
+                throw new FileNotFoundException("파일을 찾을 수 없습니다.", fileName);
+            }
+        }
+        public static bool TryLoad(string fileName, out WebtoonInfoCollection info)
+        {
+            if(File.Exists(fileName))
+            {
+                using(Stream rs = new FileStream(fileName, FileMode.Open))
+                {
+                    BinaryFormatter deserializer = new BinaryFormatter();
+                    info = (WebtoonInfoCollection)deserializer.Deserialize(rs);
+                }
+                return true;
+            }
+            else
+            {
+                info = null;
+                return false;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return this.GetType().ToString();
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 19;
+            for(int i = 0 ; i < this.Count ; i++)
+            {
+                hash += hash * 31 + this[i].GetHashCode();
+            }
+            return hash;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(this.GetType() == obj.GetType())
+            {
+                return this.GetHashCode() == obj.GetHashCode();
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator ==(WebtoonInfoCollection left, object right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(WebtoonInfoCollection left, object right)
+        {
+            return left.Equals(right);
+        }
+    }
+}
