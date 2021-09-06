d5138ae673205bf556457c128de4ab1c52d5863d
kdm1jkm <kdm1jkmm@gmail.com>
2021-09-06 11:04:35 +0900

    라이브러리 테스트용 콘솔 프로젝트 추가

---
+++ ConsoleWebtoonDownloader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleWebtoonDownloader
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Hello, World!");
+        }
+    }
+}
